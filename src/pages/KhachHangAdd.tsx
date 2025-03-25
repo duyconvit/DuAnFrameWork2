6cf7a0a5d489843c08f215a59a75b82c2e2b6806
toanltph49436 <toanltph49436@gmail.com>
2025-03-25 12:26:54 +0700

    toancommit

---
+++ src/pages/KhachHangAdd.tsx
@@ -0,0 +1,10 @@
+import { Layout, Menu, Table, Button, Form, Input, message, Modal } from "antd";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const { Header, Sider, Content } = Layout;
+
+
+export default CustomerList;
